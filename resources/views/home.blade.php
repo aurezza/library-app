3f5c3f0f65aee66fe0f7ba4dc0c898c4dc984013
Aurezza <aurezza@gmail.com>
2018-12-20 21:00:09 +0800

    added view, styling and home controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,44 +9,21 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('css/custom.css')}}" rel="stylesheet" type="text/css"/>
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
         
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    A Simple Library App
                 </div>
 
-                <ul class="book-list">
-                @foreach ($books as $book)
-                    <li>
-                        <img src="https://placeimg.com/150/200/any" height="150" width="200"><br>
-                        <p><strong>{{ $book->name }}</strong>, <i>{{ $book->number_of_pages }} pages</i></p>
-                        @if ($book->borrowed_by)
-                            <p>Borrowed by: {{ $book->borrowed_by}}</p>
-                            <p>Borrowed at: {{ $book->borrowed_at}}</p>
-                        @else
-                        <p>Borrow this book (input your id)</p>
-                        <form action="/api/books/borrow/{{$book->id}}" method="post">
-          <label for="id">Your ID</label>
-          <input class="form-control" type="number" name="id" id="id" value="">
-        </div>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Submit</button>
-     
-  </form>
-                        @endif
-
-
-                        @if ($book->returned_at)
-                            <p>Returned at: {{ $book->returned_at}}</p>
-                        @endif 
-                        
-                    </li>
-                @endforeach
-                </ul>
+                 <div class="links">
+                    <a href="{{ url('/books') }}">Books</a>
+                    <a href="{{ url('/students') }}">Students</a>
+                </div>
             </div>
         </div>
     </body>
