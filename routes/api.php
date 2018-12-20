812692d49de646d005445f0e8f953349a30586b7
Aurezza <aurezza@gmail.com>
2018-12-20 20:59:48 +0800

    added dispatch routes

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,14 @@ use Illuminate\Http\Request;
 Route::get('books', 'BookController@index');
 Route::get('books/{id}', 'BookController@show');
 Route::post('books', 'BookController@store');
-Route::put('books/{id}/{name}/{numberOfPages}', 'BookController@update');
+Route::put('books/{id}', 'BookController@update');
 Route::delete('books/{id}', 'BookController@delete');
 Route::post('books/borrow/{bookId}', 'BookController@borrow');
-Route::post('books/returnBook/{id}', 'BookController@returnBook');
+Route::post('books/returnBook/{bookId}', 'BookController@returnBook');
 
 // User routes
 Route::get('users', 'UserController@index');
 Route::get('users/{id}', 'UserController@show');
 Route::post('users', 'UserController@store');
-Route::put('users/{id}/{name}/{studentNumber}', 'UserController@update');
+Route::put('users/{id}', 'UserController@update');
 Route::delete('users/{id}', 'UserController@delete');
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,69 @@
 |
 */
 
-Route::get('/', function () {
+use App\User;
+use Illuminate\Support\Facades\Input;
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/books', function () {
     $request = Request::create('/api/books/', 'GET');
 
     $response = Route::dispatch($request);
     $body = $response->getOriginalContent();
     $books = json_decode($body);
 
-    return view('home', compact('books'));
+    return view('books', compact('books'));
+});
+
+Route::get('/students', function () {
+    $request = Request::create('/api/users/', 'GET');
+
+    $response = Route::dispatch($request);
+    $body = $response->getOriginalContent();
+    $users = json_decode($body);
+
+    return view('students', compact('users'));
+});
+
+Route::post('/students/add', function() {
+  
+    $request = Request::create(
+        '/api/users/',
+        'POST',
+        Input::get());
+
+    $response = Route::dispatch($request);
+    
+    
+    $users = User::all();
+    return view('students', compact('users'));
+});
+
+Route::put('/students/edit/{id}', function() {
+    $request = Request::create(
+        '/api/users/{id}',
+        'PUT',
+        Input::get());
+
+    // $res = app()->handle($request);
+    $response = Route::dispatch($request);
+     
+    $users = User::all();
+    return view('students', compact('users'));
+});
+
+Route::delete('/students/delete/{id}', function() {
+    $request = Request::create(
+        '/api/delete/{id}',
+        'DELETE');
+  
+    // $res = app()->handle($request);
+    $response = Route::dispatch($request);
+     if($response) {
+         $users = User::all();
+    return view('students', compact('users'));
+     }
+   
 });
 
