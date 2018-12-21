b74d35ba319a6924014d2089b921dc52008cade8
aurezza <aldunque@tmj.ph>
2018-12-21 12:19:26 +0800

    adjusted nav

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,23 +12,7 @@
         @yield('custom-script')
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="{{url('/')}}">Library System</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{url('/students')}}">Students</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{url('/books')}}">Books</a>
-                </li>
-                </ul>
-            </div>
-        </nav>
-    
+        @include('layout.navbar')
         <div id="wrapper">
             <div id="sidebar-wrapper">
                 @yield('sidebar')
